5e1222d0c61c24c27b2b45ded867ef77dee03582
tim48-robot <justintimothy303@gmail.com>
2025-11-29 14:58:40 +0700

    build

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:courtify_mobile/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
-}
+}/////
 //
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
